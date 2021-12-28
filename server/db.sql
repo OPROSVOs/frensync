6943d69d5da7bbfad887bf67610f96ecd3587a9e
/OPROS/VOs <lukhab20@gmail.com>
2021-12-28 15:56:03 +0000

    Update db.sql

--- server/db.sql
+++ server/db.sql
@@ -1,14 +1,23 @@
 /* DB Schema: */
-create table s if not exists(
-        p INT UNSIGNED NOT NULL PRIMARY KEY,
-        t INT UNSIGNED NOT NULL,
-        b ENUM('b', 'soc', 's4s', 'trash') NOT NULL,
-        n VARCHAR(128) NULL,
-        tr VARCHAR(64) NULL,
-        s VARCHAR(128) NULL,
-        e VARCHAR(128) NULL,
-        dnt VARCHAR(2) NULL
-);
+
+DROP TABLE IF EXISTS `s`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `s` (
+  `t` int unsigned NOT NULL,
+  `p` int unsigned NOT NULL,
+  `b` enum('b','soc','s4s','trash') NOT NULL,
+  `n` varchar(128) DEFAULT NULL,
+  `tr` varchar(24) DEFAULT NULL,
+  `s` varchar(128) DEFAULT NULL,
+  `e` varchar(128) DEFAULT NULL,
+  `dnt` varchar(1) DEFAULT NULL,
+  `ch` smallint DEFAULT NULL,
+  `ca` smallint DEFAULT NULL,
+  PRIMARY KEY (`p`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
 /* 
  max int value: 4294967295
  bigint might be better?

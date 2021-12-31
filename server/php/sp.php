394cd00021122a3242a21d3b6ada04f4fe365b1c
/OPROS/VOs <lukhab20@gmail.com>
2021-12-31 06:55:04 +0000

    Update sp.php

--- server/php/sp.php
+++ server/php/sp.php
@@ -1,3 +1,4 @@
+
 <?php
 
 // CORS
@@ -5,8 +6,19 @@ header('Access-Control-Allow-Origin: https://boards.4chan.org');
 header('Access-Control-Allow-Credentials: true');
 header('Access-Control-Allow-Headers: x-requested-with, if-modified-since');
 header('Content-type: text/html; charset=utf-8');
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {exit("ok");}
 
 error_reporting (E_ALL ^ E_NOTICE);
+
+include 'FloodProtection.php';
+$myname = basename(__FILE__);
+$FloodProtection = new FloodProtection($myname, 5, 60); #4 valid requests within 60sec
+if($FloodProtection->check($_SERVER['REMOTE_ADDR'])){
+    header("HTTP/1.1 429 Too Many Requests");
+    #error_log("FloodProtection ".$myname.": (".$_SERVER['REMOTE_ADDR'].")");
+    exit("1");
+}
+
 
 function test_input($data) {
   #$data = trim($data);
@@ -14,34 +26,67 @@ function test_input($data) {
   $data = htmlspecialchars($data);
   return $data;
 }
-function e($error, $text) {
-        rawurlencode($error);
-        error_log("php:".$text . " " . $error);
-}
 
 // Grab the input and bring it to a safe state
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-$p = test_input($_POST["p"]);
-$t = test_input($_POST["t"]);
-$b = test_input($_POST["b"]);
-$n = $_POST["n"];
-$s = test_input($_POST["s"]);
-$e = test_input($_POST["e"]);
-$dnt = test_input($_POST["dnt"]);
+    $p = test_input($_POST["p"]);
+    $t = test_input($_POST["t"]);
+    $b = test_input($_POST["b"]);
+    $n = $_POST["n"];
+    $s = test_input($_POST["s"]);
+    $e = test_input($_POST["e"]);
+    $dnt = test_input($_POST["dnt"]);
+    $ca = test_input($_POST["ca"]);
+    $ch = test_input($_POST["ch"]);
+}
+else{
+    header("HTTP/1.1 405 Method Not Allowed");
+    exit("nope");
 }
 // input range check
-if(!isset($p) or !is_numeric($p) or $p < 1 or $p > 4294967295 or !isset($t) or !is_numeric($t) or $t < 1 or $t > 4294967295 ){exit();}
-if(!isset($b) or strlen($b) > 5 or strlen($n) > 256 or strlen($s) > 128 or strlen($e) > 128 or strlen($dnt) > 4){exit();}
+if(!isset($p) or !is_numeric($p) or $p < 1 or $p > 4294967295 ){header("HTTP/1.1 406 Not Acceptable");exit("A");}
+if(!isset($t) or !is_numeric($t) or $t < 1 or $t > 4294967295 ){header("HTTP/1.1 406 Not Acceptable");exit("B");}
+if(!isset($b) or strlen($b) > 5 ){header("HTTP/1.1 406 Not Acceptable");exit("C");}
+if(strlen($n) > 256 or strlen($s) > 128 or strlen($e) > 128 or strlen($dnt) > 4){header("HTTP/1.1 406 Not Acceptable");exit("D");}
+if(!isset($ca) or $ca <= 0 or $ca > 100 or $ch > 360 or $ch < 0 ){$ca = 0;$ch = 0;}
 
 $name = explode('#', $n, 2);
+$trip = "";
 if($name[1]){
-    //Works with wide, non ASCII characters
-    $name[1] = htmlspecialchars($name[1]); 
-    $num_chars = iconv_strlen($name[1], "UTF-8");
+    $trip = make_tripcode($name[1]);
+}
+
+$db = new PDO('mysql:host=localhost;dbname=ns', 'ns', 'ns' );
+
+// escape ' to \' to prevent the obvious
+$p = $db->quote($p);
+$t = $db->quote($t);
+$b = $db->quote($b);
+$n = $db->quote(htmlspecialchars($name[0]));
+$trip = $db->quote(htmlspecialchars($trip));
+$s = $db->quote($s);
+$e = $db->quote($e);
+$dnt = $db->quote($dnt);
+$ca = $db->quote($ca);
+$ch = $db->quote($ch);
+
+$sql = "INSERT INTO s (p, t, b, n, tr, s, e, dnt, ca, ch) VALUES ( {$p}, {$t}, {$b}, {$n}, {$trip}, {$s}, {$e}, {$dnt}, {$ca}, {$ch} ); ";
+#echo "SQL:   $sql ";
+echo $db ->exec($sql);
+//if(!($b == "b" or $b == "trash" or $b == "s4s" or $b == "soc")){echo "nope";exit();}
+
+
+
+function make_tripcode($pass) {
+    if ($pass === NULL || $pass === "") {
+        return "";
+    }
+    $pass = htmlspecialchars($pass); // don't replace apostrophes
+    $num_chars = iconv_strlen($pass, "UTF-8");
     $conv = "";
     for ($i = 0; $i < $num_chars; $i++) {
         try {
-            $conv .= iconv("UTF-8", "CP932", iconv_substr($name[1], $i, 1, "UTF-8"));
+            $conv .= iconv("UTF-8", "CP932", iconv_substr($pass, $i, 1, "UTF-8"));
         }
         catch (Exception $ex) {
             $conv .= "?";
@@ -49,23 +94,10 @@ if($name[1]){
     }
     $salt = strtr(preg_replace("/[^\.-z]/", ".", substr($conv . "H.", 1, 2)), ":;<=>?@[\\]^_`", "ABCDEFGabcdef");
     $trip = substr(crypt($conv, $salt), -10);
-    $name[0] .= "!" . $trip;
+    return "!" . $trip;
 }
 
-$fname = "tmp/ns-$b-$t.json";
-$db = new PDO('mysql:host=localhost;dbname=ns', 'ns', 'nope' );
 
-// escape ' to \' to prevent the obvious
-$p = $db->quote($p);
-$t = $db->quote($t);
-$b = $db->quote($b);
-$n = $db->quote(htmlspecialchars($name[0]));
-$s = $db->quote($s);
-$e = $db->quote($e);
-$dnt = $db->quote($dnt);
-
-$sql = "INSERT INTO s (p, t, b, n, s, e, dnt) VALUES ( {$p}, {$t}, {$b}, {$n}, {$s}, {$e}, {$dnt} ); ";
-echo "SQL:   $sql ";
-echo $db ->exec($sql);
 
 ?>
+
